4a1ac923bcdbeee67a31e85519082714e9a14d2b
Jennifer Manriquez <jennymanb@gmail.com>
2021-10-15 11:28:47 -0500

    Add titles in Gallery

--- components/Artwork/Artwork.js
+++ components/Artwork/Artwork.js
@@ -6,12 +6,14 @@ import Link from 'next/link';
 
 const Artwork = (props) => {
   const [imageSource, setImageSource] = useState('https://pm-site-assets-py4.s3.us-east-2.amazonaws.com/products/legacy/gray_smoke_sq1_800p.jpg');
-  const [title, setTitle] = useState("Display title of the work");
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
 
   useEffect(() => {
   fetchData(parseInt(props.object)).then(data => {
     setImageSource(data.primaryImageSmall);
     setTitle(data.title);
+    setAuthor(data.artistDisplayName.slice(0,33));
 
   }).catch(err => console.error(err))
   }, [])
@@ -21,7 +23,8 @@ const Artwork = (props) => {
       <Link href={`/artworks/${parseInt(props.object)}`}>
         <img src={`${imageSource}`} />
       </Link>
-      <div className={styles.bottomLeft}>{title}</div>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.author}>{author}</div>
     </div>
   )
 }
